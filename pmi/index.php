4aec8bc2d4966b7747628a9914dbd4fdb78ce814
supriyo15998 <supriyo15998@gmail.com>
2019-11-08 12:48:23 +0530

    Registration issue fixed

--- pmi/index.php
+++ pmi/index.php
@@ -1,49 +1,78 @@
-<?php
+<?php 
+include('server.php'); 
+//$errors = array();
+if(isset($_POST['register'])){
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']); 
 
-session_start();
-
-if(isset($_SESSION['username'])){
-    $_SESSION['msg'] = "you must log in first to view this page";
-    header("location: login.php");
+    if (empty($username) or empty($email) or empty($password_1) or empty($password_2)) {
+          ?>
+            <script type="text/javascript">
+                alert('Empty fields found!');
+            </script>
+          <?php
+          die(mysqli_error($db));
+          header("Location: index.php");
+      }  
+      
+    else {
+        if($password_1 == $password_2)
+        {
+            $password = md5($password_1);
+            $query = "INSERT INTO users (username, email, password) VALUES ('$username','$email','$password')";
+            $exec = mysqli_query($db, $query);
+            ?>
+                <script type="text/javascript">
+                    alert('User created successfully!');
+                </script>
+            <?php
+        }
+        else{
+            ?>
+                <script type="text/javascript">
+                    alert('Two passwords should match');
+                    die(mysqli_error($db));
+                    header("Location:index.php");
+                </script>
+            <?php
+        }
+    }    
 }
-
-if(isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['username']);
-    header("location: login.php");
-}
-
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <title> Home Page</title>
-</head>
-<body>
+    <head>
+        <title>
+            Registration Page
+        </title>
+    </head>
 
-    <h1> This is a messsage</h1>
-    <?php
-    if(isset($_SESSION['success'])) : ?>
+    <body>
+        <h1>Register</h1>
+        <form action = "index.php" method = "POST">
+        
+            <div>
+                <label for="username">Username</label>
+                <input type = "text" placeholder="Username" name="username"/> 
+            </div>
+            <div>
+                    <label for="email">E-mail</label>
+                    <input type = "email" placeholder="E-mail" name="email"/> 
+            </div>
+            <div>
+                    <label for="password">Password</label>
+                    <input type = "password" placeholder="Password" name="password_1"/>
+            </div>
+            <div>
+                    <label for="password">Confirm Password</label>
+                    <input type = "password" placeholder="Confirm Password" name="password_2"/>
+            </div>
+            <button type="submit" name="register">Submit</button>
+            <a href="login.php">already a member</a>
+        </form>
+    </body>
 
-    <div>
-    <h3>
-    <?php
 
-    echo $_SESSION['success'];
-    unset($_SESSION['success']);
-
-    ?>
-    </h3>
-    </div>
-    <?php endif ?>
-
-    <?php if(isset($_SESSION['username'])) : ?>
-
-    <h3> Welcome <strong><?php echo $_SESSION['username']; ?></strong></h3>
-    <button><a href = "index.php?logout = '1'"></a></button>
-
-    <?php endif ?>
-
-</body>
 </html>
